7d7e9a28d6a8991438115aede868d36d904ae997
miragasgerov <miraga.esgerli90@gmail.com>
2022-11-02 02:31:52 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,122 @@
+// Question 1
+let number1 = 14;
+let number2 = 4;
+console.log(number1 + " " + "and" + " " + number2);
+
+// Question2
+
+let strOne = "Miraga";
+let strTwo = "Asgarli";
+let strThree = "Baku";
+
+alert(strOne + " " + strTwo + " " + strThree);
+
+// Question3
+
+let num1 = 20;
+let str1 = "Miraga";
+let arr1 = ["Miraga","Asgarli"];
+let func1 = function(){};
+
+console.log(typeof(num1) + " " + typeof(str1) + " " + typeof(arr1) + " " + typeof(func1));
+
+// ve yaxud 
+
+let func1TypeOf = typeof(func1);
+let num1TypeOf = typeof(num1);
+let str1TypeOf = typeof(str1);
+let arr1TypeOf = typeof(arr1);
+
+console.log(num1TypeOf + " " + str1TypeOf + " " + arr1TypeOf + " " + func1TypeOf);
+
+
+// Question 4 
+
+let x = 19;
+let a = 21;
+
+console.log("x" + " " + "+" + " " + "a" + " " + "=" + " " + (x+a));
+
+// Question 5
+
+let z = 10;
+let t = "Miraga";
+let l = z+t;
+
+console.log(typeof(l));
+
+
+// Question 6
+
+let m = 10;
+let s = "10";
+let ms = m + s;
+
+console.log(ms + " " + typeof(ms));
+
+
+// Question 7
+
+let y;
+y = 50;
+console.log(y);
+
+// Question 8
+
+let fruitName;
+fruitName = "Orange";
+
+console.log(fruitName);
+
+// Question 9
+
+let b = 3;
+let c = 10;
+
+console.log(b+c);
+
+
+// Question 10
+
+
+let letterOne = 10;
+let letterTwo = 20;
+let letterThree = letterOne + letterTwo;
+
+alert(letterThree);
+
+// Question 11
+
+let human = {
+    firstname : "Miraga",
+    lastname : "Asgarli",
+    age : 20
+
+};
+
+
+// Question 12
+
+
+let bc = 5 + 7;
+alert(bc);
+
+
+// Question 13
+
+
+let bp = 13/7;
+alert(bp + " " + "yuvarlaqlasdirsaq" + " " + Math.round(bp));
+
+
+// Question14
+// Question15
+
+// Question 16
+
+let numberOne = 5;
+let StringOne = "Miraga";
+let ArrOne = [];
+let FuncOne = function(){};
+
+console.log(numberOne + StringOne + ArrOne + FuncOne + " " + "typeOf:" + " " + (typeof(numberOne+StringOne+ArrOne+FuncOne)))
